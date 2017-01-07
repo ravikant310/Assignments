7c01979e3ab9a8ff86574f7e924a0d444b0d53de
ravikant310 <ravikantgupta310@gmail.com>
2017-01-07 10:04:27 +0530

    Task03

---
+++ script/jscript.js
@@ -0,0 +1,36 @@
+/*var name="ravi";
+console.log(name);*/
+/*function scream(name)
+{
+	var i=0;
+	alert("your name is  " +name);
+	while(i<500)
+	{
+		console.log("name :" +name)
+		i++;
+	}
+}*/
+/*String x="somestring";*/
+function sbstring(s)
+{
+	    var i;
+	    var j;
+   	    /*String temp;
+*/
+            for ( i = 0;  i < s.length - 1;  i++ )
+            {
+                for ( j = i + 1;  j < s.length;  j++ )
+                {  
+                         /*if ( s [ i ].compareTo( s [ j ] ) > 0 )*/
+                         if ( s [ i ]!=s [ j ] )
+                          {                                            
+                                      console.log("not square");
+                                                
+                           } 
+						 else
+							{
+							console.log("square");
+							}
+                } 
+             } 
+}
